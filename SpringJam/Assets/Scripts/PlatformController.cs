3f9de27dcfce6c28d4a2435e24daf33351f40753
RJ2004169 <90154107+RJ2004169@users.noreply.github.com>
2023-05-03 23:25:26 +0100

    Platform movement, generation and tracking fixed
    Platforms generated and moving down constantly. Gets deleted after it exists the camera view

--- SpringJam/Assets/Scripts/PlatformController.cs
+++ SpringJam/Assets/Scripts/PlatformController.cs
@@ -5,12 +5,14 @@ using UnityEngine;
 public class PlatformController : MonoBehaviour
 {
 
+    private const float platformMovementSpeed = -5f;
+
     private static float CurrentTopYPosition = -5.0f;
     private static double CurrentPlatformX = -3.0f;
-    private static bool GeneratePlatform = false;
+    //private static bool GeneratePlatform = false;
     private System.Random random = new System.Random();
     public List<GameObject> platformList = new List<GameObject>();
-    private Vector3 currentPlatformPosition = new Vector3();
+    private Rigidbody2D currentPlatformPosition = new Rigidbody2D();
 
     public GameObject platformPrefab;
     public GameObject groundPrefab;
@@ -18,15 +20,10 @@ public class PlatformController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-                          platformList.Add(groundPrefab);
+        platformList.Add(groundPrefab);
         while (CurrentTopYPosition < 4.8)
         {
             GenerateRandomPlatform();
-
-            if (GeneratePlatform)
-            {
-                createPlatform();
-            }
         }
 
     }
@@ -34,48 +31,53 @@ public class PlatformController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //moveAllPlatforms();           MOVED TO FIXEDUPDATE()
+        //while (CurrentTopYPosition < 4.8)
+        //{
+        //    GenerateRandomPlatform();
+
+        //}
+
+    }
+
+    private void FixedUpdate()
+    {
         moveAllPlatforms();
-        //CurrentTopYPosition -= 0.5f;
         while (CurrentTopYPosition < 4.8)
         {
             GenerateRandomPlatform();
-            if (GeneratePlatform)
-            {
-                createPlatform();
-            }
-        }
 
+        }
     }
     private void moveAllPlatforms()
     {
-        foreach (GameObject platform in platformList)
+        for (int i = 0; i<platformList.Count; i++)//GameObject platform in platformList)
         {
-            if (platform.transform.position.y < -5.0f)
+            if (platformList[i] != null)
             {
-                Destroy(platform);
-                platformList.Remove(platform);
-            }
-            else
-            {
-                currentPlatformPosition = platform.transform.position;
-                platform.transform.position.Set(currentPlatformPosition.x, currentPlatformPosition.y - 0.5f, currentPlatformPosition.z);
+                if (platformList[i].transform.position.y < -5.0f)
+                {
+                    //platformList.Remove(platformList[i]);
+                    Destroy(platformList[i]);
+                    platformList.RemoveAll(s => s == null);
+                    
+                }
+                else
+                {
+                    currentPlatformPosition = platformList[i].GetComponent<Rigidbody2D>();
+                    currentPlatformPosition.MovePosition((Vector2)platformList[i].transform.position + new Vector2(0.0f, platformMovementSpeed) * Time.deltaTime);
+                    //platform.transform.position.Set(currentPlatformPosition.x, currentPlatformPosition.y - 0.5f, currentPlatformPosition.z);
+                }
             }
         }
-        //CurrentTopYPosition -= 0.5f;
+        CurrentTopYPosition = platformList[platformList.Count-1].transform.position.y;
 
     }
 
-    private void createPlatform()
+    public void GenerateRandomPlatform()
     {
-        GeneratePlatform = false;
+        CurrentTopYPosition += 4.0f;
         CurrentPlatformX = random.NextDouble() * (6.7f) - 3.0f; //min and max bounds for x hardcoded to generate random starting position for platforms
-        platformList.Add((GameObject) Instantiate(platformPrefab, new Vector3((float)CurrentPlatformX, CurrentTopYPosition, 0.0f), Quaternion.identity));
-    }
-
-    public static void GenerateRandomPlatform()
-    {
-        CurrentTopYPosition += 2.0f;
-        CurrentPlatformX += 0.7f;
-        GeneratePlatform = true;
+        platformList.Add((GameObject)Instantiate(platformPrefab, new Vector3((float)CurrentPlatformX, CurrentTopYPosition, 0.0f), Quaternion.identity));
     }
 }
